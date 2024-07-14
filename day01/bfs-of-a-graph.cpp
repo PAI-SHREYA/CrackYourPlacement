5f56e52c56bb3997a99fe72a61281f1d365e4964
Shreya Pai <105111079+PAI-SHREYA@users.noreply.github.com>
2024-07-14 20:52:56 +0530

    Update bfs-of-a-graph.cpp

--- day01/bfs-of-a-graph.cpp
+++ day01/bfs-of-a-graph.cpp
@@ -1,20 +1,29 @@
 class Solution {
-public:
-    ListNode* reverseList(ListNode* head) {
-      ListNode* prev=nullptr;
-      ListNode* temp=head;
-      ListNode* curr=head;
-      while(curr)
-      {
-          
-         temp=temp->next; 
-         curr->next=prev;
-         prev=curr;
-         
-         curr=temp;
-      }
-      head=prev;
-      return head ;
+  public:
+    // Function to return Breadth First Traversal of given graph.
+    vector<int> bfsOfGraph(int V, vector<int> adj[]) {
+        
+        // Code here
+        vector<int> vis(V,0),res;
+        queue<int> bfs;
+        vis[0]=1;
+        bfs.push(0);
+        while(!bfs.empty())
+        {
+            int node=bfs.front();
+            bfs.pop();
+            res.push_back(node);
+            for(int i=0;i<adj[node].size();i++)
+            {
+                
+                if(!vis[adj[node][i]])
+                { 
+                    vis[adj[node][i]]=1;
+                    bfs.push(adj[node][i]);
+                    
+                }
+            }
+        }
+        return res;
     }
-
 };
