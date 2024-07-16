8c1804ae2f25347457318b2e3e49c90d53af343e
Shreya Pai <105111079+PAI-SHREYA@users.noreply.github.com>
2024-07-16 10:41:40 +0530

    Update find-all-duplicates-in-an-array.cpp

--- day03/find-all-duplicates-in-an-array.cpp
+++ day03/find-all-duplicates-in-an-array.cpp
@@ -4,8 +4,10 @@ public:
         vector<int> ans;
         for(int i=0;i<nums.size();i++)
         {
-            nums[nums[i]-1]=-1*nums[nums[i]-1];
-            if(nums[nums[i]-1]>0) ans.push_back(nums[i]);
+            int index= abs(nums[i]);
+            if(nums[index-1]<0) ans.push_back(abs(nums[i]));
+            else nums[index-1]= -nums[index-1];
+            //if(nums[nums[i]-1]>0) ans.push_back(nums[i]);
         }
 
         return ans;
