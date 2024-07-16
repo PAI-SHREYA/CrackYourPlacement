a32100f97f46fec4376234f6f790b283003394fb
Shreya Pai <105111079+PAI-SHREYA@users.noreply.github.com>
2024-07-16 10:23:07 +0530

    Create best-time-to-buy-and-sell-stock.cpp

---
+++ best-time-to-buy-and-sell-stock.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+         int buy=prices[0],profit=0;
+         for(int i=1;i<prices.size();i++)
+         {
+            profit=max(profit,prices[i]-buy);
+            if(buy>prices[i]) buy=prices[i];
+            
+         }
+         return profit;
+
+        
+    }
+};
