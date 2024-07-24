79e8a0a6557fef61bab71786afc87577d605fbbf
Shreya Pai <105111079+PAI-SHREYA@users.noreply.github.com>
2024-07-24 11:41:49 +0530

    Create diameter-bt.cpp

---
+++ day11/diameter-bt.cpp
@@ -0,0 +1,31 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    int maxi=0;
+    int diameterOfBinaryTree(TreeNode* root) {
+        int maxi=0;
+        dia(root,maxi);
+        return maxi;
+        
+        
+    }
+    int dia(TreeNode* root, int & maxi)
+    {
+        if(root==nullptr) return 0;
+        int left=dia(root->left,maxi);
+        int right=dia(root->right,maxi);
+        maxi=max(maxi,left+right);
+        return 1+ max(left,right);
+
+    }
+};
