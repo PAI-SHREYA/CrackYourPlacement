56b82346a3c507d11f0570a92f647182611c5f47
Shreya Pai <105111079+PAI-SHREYA@users.noreply.github.com>
2024-07-30 22:31:09 +0530

    Create Gas Station.cpp

---
+++ day17/Gas Station.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int canCompleteCircuit(vector<int>& gas, vector<int>& cost) {
+        int total=0,current=0,start=0;
+        for(int i=0;i<gas.size();i++ )
+        {
+            total+=gas[i]-cost[i];
+            current+=gas[i]-cost[i];
+            if(current<0)
+            {
+                current=0;
+                start=i+1;
+            }
+        }
+        if(total<0) return -1;
+        return start;
+        
+    }
+};
